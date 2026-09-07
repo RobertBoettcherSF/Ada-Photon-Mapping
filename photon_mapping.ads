9479d291693689683637d3308b4d70b31d831d5f
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-09-07 21:36:04 +0200

    Fix array initialization syntax in Query_Result

--- photon_mapping.ads
+++ photon_mapping.ads
@@ -59,7 +59,7 @@ package Photon_Mapping with SPARK_Mode => On is
 
    type Query_Result (Max_Results : Natural) is record
       Photons         : Photon_Array (1 .. Max_Results);
-      Distances_Sq    : Distance_Array (1 .. Max_Results) := (others => 0.0);
+      Distances_Sq    : Distance_Array (1 .. Max_Results) := [others => 0.0];
       Count           : Natural := 0;
       Max_Distance_Sq : Real    := 0.0;
    end record;
